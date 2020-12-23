a173fc7afdb131135183d65c0924060a871e4b55
mkomod <mkomodromos01@gmail.com>
2020-12-23 18:02:04 +0000

    fix: binary search

--- R/sCCA.r
+++ R/sCCA.r
@@ -47,19 +47,20 @@ sCCA <- function(X1, X2, l1, l2, iteration=1000, threshold=1e-6) {
 sCCA.binary_search <- function(v, l, iteration=150) {
     
     d.lower <- 0
-    d.upper <- max(abs(v))
+    d.upper <- max(abs(v))             # soft threshold of this val is
+    # <= 0 forall elements in w
 
     while (iteration) {
 	w <- normalise.vect(soft.thresh(v, (d.upper + d.lower) / 2))
 	if (l1.norm(w) < l) {
-	   d.upper <-  (d.lower + d.upper) / 2
-	} else {
 	   d.lower <-  (d.lower + d.upper) / 2
+	} else {
+	   d.upper <-  (d.lower + d.upper) / 2
 	}
 
-	if (d.upper - d.lower < 1e-6) return((d.upper - d.lower) / 2)
+	if (d.upper - d.lower < 1e-6) return((d.upper + d.lower) / 2)
 	iteration <- iteration - 1
     }
 
-    return((d.upper - d.lower) / 2)
+    return((d.upper + d.lower) / 2)
 }
