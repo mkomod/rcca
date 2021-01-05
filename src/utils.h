25d4912f0449388a694d3214e97abd6dc00f1c00
mkomod <mkomodromos01@gmail.com>
2021-01-05 11:25:57 +0200

    rm: cpp code and refactor

--- src/utils.h
+++
@@ -1,8 +0,0 @@
-#ifndef PKG_UTILS_H
-#define PKG_UTILS_H
-
-double binary_search(arma::colvec v, double l, u_int niter = 1000,  
-	double threshold = 1.0e-6);
-arma::colvec soft_threshold(arma::colvec v, double d);
-
-#endif
